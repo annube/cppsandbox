bfbe9328cea0c63bfc50d41d882c7cbfdb64f747
Andreas Ammon <aam@linuxvmaam>
2020-11-06 14:56:44 +0100

    add graph parse example

---
+++ graph_parse/main.cpp
@@ -0,0 +1,16 @@
+#include <boost/property_tree/ptree.hpp>
+#include <optional>
+
+int main(int argc, char *argv[]){
+
+
+    std::optional<std::string> filename;
+
+    if(argc > 1)
+    {
+        filename = argv[1];
+    }
+
+
+
+}
