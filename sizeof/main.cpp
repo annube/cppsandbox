8c5130fb50045b0f4b8de2aa99ac4cf268aacb5c
Andreas Ammon <nubi@freenet.de>
2023-05-17 17:57:14 +0200

    add non-trivial example for usage of sizeof

--- sizeof/main.cpp
+++ sizeof/main.cpp
@@ -9,10 +9,18 @@ void printSize() {
   std::printf("%s %ld\n", typeInfo.pretty_name().c_str(), sizeof(T));
 }
 
+struct S
+{
+  int x;
+  char c;
+  int z;
+};
+
 int main()
 {
   printSize<long>();
   printSize<long long int>();
   printSize<unsigned int>();
   printSize<double>();
+  printSize<S>();
 }
