aa5c55de621d9e66f6088273ee97b6e2088cfde3
Andreas Ammon <andreas.ammon@siemens.com>
2024-04-22 15:16:58 +0200

    modularize server client connection handling

--- tls_server/tls_server.c
+++ tls_server/tls_server.c
@@ -12,6 +12,12 @@
        __typeof__ (b) _b = (b); \
      _a > _b ? _a : _b; })
 
+
+#define min(a,b) \
+({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a < _b ? _a : _b; })
+
 int create_socket(int port)
 {
     int s;
@@ -40,7 +46,7 @@ int create_socket(int port)
     return s;
 }
 
-SSL_CTX *create_context()
+SSL_CTX *create_ssl_server_context()
 {
     const SSL_METHOD *method;
     SSL_CTX *ctx;
@@ -57,25 +63,84 @@ SSL_CTX *create_context()
     return ctx;
 }
 
-struct SSL_config
+typedef struct _ssl_server_config
 {
     char server_certificate[1024];
     char server_key[1024];
-};
+} ssl_server_config;
 
-
-void configure_context(SSL_CTX *ctx, struct SSL_config *sslConfig)
+typedef struct _ssl_server_context
 {
+    SSL_CTX* ssl_ctx;
+    ssl_server_config* cfg;
+    int socketFD;
+} ssl_server_context;
+
+typedef struct _tls_context
+{
+
+    SSL *ssl;
+    int clientFD;
+    struct sockaddr_in addr;
+    ssl_server_context* server_ctx;
+
+} ssl_client_connection;
+
+
+void init_context(ssl_server_context* ssl_server_ctx, ssl_server_config* sslConfig)
+{
+
+    ssl_server_ctx->ssl_ctx = create_ssl_server_context();
+
+    ssl_server_ctx->cfg=sslConfig;
+
     /* Set the key and cert */
-    if (SSL_CTX_use_certificate_file(ctx, sslConfig->server_certificate, SSL_FILETYPE_PEM) <= 0) {
+    if (SSL_CTX_use_certificate_file(ssl_server_ctx->ssl_ctx, sslConfig->server_certificate, SSL_FILETYPE_PEM) <= 0) {
         ERR_print_errors_fp(stderr);
         exit(EXIT_FAILURE);
     }
 
-    if (SSL_CTX_use_PrivateKey_file(ctx,  sslConfig->server_key, SSL_FILETYPE_PEM) <= 0 ) {
+    if (SSL_CTX_use_PrivateKey_file(ssl_server_ctx->ssl_ctx,  sslConfig->server_key, SSL_FILETYPE_PEM) <= 0 ) {
         ERR_print_errors_fp(stderr);
         exit(EXIT_FAILURE);
     }
+}
+
+int parse_ssl_config(int argc, char* argv[], ssl_server_config* sslConfig)
+{
+   if(argc < 3){
+       return -1;
+   }
+    memset(sslConfig, '\0', sizeof(sslConfig));
+    strcpy(sslConfig->server_key, argv[1]);
+    strcpy(sslConfig->server_certificate, argv[2]);
+    return 0;
+}
+
+
+int nextClientRequest(ssl_server_context* ssl_server_ctx, ssl_client_connection* client_conn)
+{
+    unsigned int len = sizeof(struct sockaddr_in);
+    client_conn->clientFD = accept(ssl_server_ctx->socketFD, (struct sockaddr*)&client_conn->addr, &len);
+    if (client_conn->clientFD < 0) {
+        perror("Unable to accept");
+        exit(EXIT_FAILURE);
+    }
+
+    client_conn->ssl = SSL_new(ssl_server_ctx->ssl_ctx);
+
+    SSL_set_accept_state(client_conn->ssl); // Server
+    SSL_set_fd(client_conn->ssl, client_conn->clientFD);
+    client_conn->server_ctx = ssl_server_ctx;
+    return 0;
+}
+
+int finishClientRequest(ssl_client_connection* client_conn)
+{
+
+    SSL_shutdown(client_conn->ssl);
+    SSL_free(client_conn->ssl);
+    close(client_conn->clientFD);
 }
 
 int main(int argc, char **argv)
@@ -87,55 +152,40 @@ int main(int argc, char **argv)
         return -1;
     }
 
-    struct SSL_config sslConfig;
+    ssl_server_config sslConfig;
 
-    memset(&sslConfig, '\0', sizeof(sslConfig));
-
-    strcpy(sslConfig.server_key, argv[1]);
-    strcpy(sslConfig.server_certificate, argv[2]);
+    parse_ssl_config(argc, argv, &sslConfig);
 
     int sock;
-    SSL_CTX *ctx;
+    ssl_server_context* ssl_server_ctx;
 
     /* Ignore broken pipe signals */
     signal(SIGPIPE, SIG_IGN);
 
-    ctx = create_context();
+    init_context(ssl_server_ctx, &sslConfig);
 
-    configure_context(ctx, &sslConfig);
-
-    sock = create_socket(4433);
+    ssl_server_ctx->socketFD = create_socket(4433);
 
     /* Handle connections */
     while(1) {
-        struct sockaddr_in addr;
-        unsigned int len = sizeof(addr);
-        SSL *ssl;
-        const char reply[] = "test\n";
 
-        int client = accept(sock, (struct sockaddr*)&addr, &len);
-        if (client < 0) {
-            perror("Unable to accept");
-            exit(EXIT_FAILURE);
-        }
+        ssl_client_connection client_conn;
 
-        ssl = SSL_new(ctx);
-        SSL_set_fd(ssl, client);
+        nextClientRequest(ssl_server_ctx, &client_conn);
 
-        if (SSL_accept(ssl) <= 0) {
+        if (SSL_do_handshake(client_conn.ssl) <= 0) {
             ERR_print_errors_fp(stderr);
         } else {
             printf("SSL Handshake accepted\n");
             char buf[1024];
-            int numBytesRead = SSL_read(ssl, buf, 1024);
-            SSL_write(ssl, buf, max(strlen(buf),numBytesRead));
+            int numBytesRead = SSL_read(client_conn.ssl, buf, 1024);
+            SSL_write(client_conn.ssl, buf, min(strlen(buf),numBytesRead));
         }
 
-        SSL_shutdown(ssl);
-        SSL_free(ssl);
-        close(client);
+        finishClientRequest(&client_conn);
+
     }
 
     close(sock);
-    SSL_CTX_free(ctx);
+    SSL_CTX_free(ssl_server_ctx->ssl_ctx);
 }
